63baa88c851b25f570ea650a50cbc89a00e8f556
yo-0ss <yoss.rios27@gmail.com>
2025-07-08 16:33:07 -0600

    Actualizado el punto de entrada principal para renderizar el nuevo componente App.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Index from './Index.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Index />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
